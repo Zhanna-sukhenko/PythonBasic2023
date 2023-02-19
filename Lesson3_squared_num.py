f1bc103c5668e3b154c9c765ea9fb5936a4be362
zhanna <sukhenkozhanna@gmail.com>
2023-02-19 16:49:11 +0200

    доопрацювала_Квадрати натуральних чисел (завдання на застосування циклу WHILE) ()

--- Lesson3_squared_num.py
+++ Lesson3_squared_num.py
@@ -1,13 +1,14 @@
-num = int(input("Enter a number: "))
-step = 1
-while step**2 <= num:
-    print(step**2, end=" ")
-    step += 1
-
 # num = int(input("Enter a number: "))
-# print(num, end = "\t") #<виводимо число на екран з параметром end="\t">
-# step = 1  # введемо змінну завдяки якій будемо перебирати числа
-# while True:
-#    if step**2 > num:
-#        break
-# print(num, end = " ") #виводимо значення step в квадраті зпараметром
+# step = 1
+# while step**2 <= num:
+#     print(step**2, end=" ")
+#     step += 1
+
+num = int(input("Enter a number: "))
+print(num, end="\t") #<виводимо число на екран з параметром end="\t">
+step = 1  # введемо змінну завдяки якій будемо перебирати числа
+while True:
+    if step**2 > num:
+        break
+    print(step**2, end=",") #виводимо значення step в квадраті зпараметром
+    step +=1
