30b0eeafaca4b0f6170575e500689dff308f2d76
zhanna <sukhenkozhanna@gmail.com>
2023-02-19 16:51:10 +0200

    Рядок та зрізи

---
+++ Lesson4_9_row_slice.py
@@ -0,0 +1,46 @@
+string = "HappyBday"
+new_string = string[0:7]
+new_string1 = string[::2]
+print("a", string[3])
+print("b", string[-2])
+print("c", string[0:5])
+print("d", new_string[0:7])
+print("e", string[1::2])
+print("f", string[0::2])
+print("g", string[::-1])
+print("h", new_string1[::-1])
+print("i", len(string))
+
+
+# string_01 = "Hello"
+# new_string = string_01[0:4]
+# # print(new_string[0:4])
+#
+# print(string_01[4:0:-1])
+
+# string_01 = "Hello"
+# #            H -5
+# #            e -4
+# #            l -3
+# #            l -2
+# #            o -1
+# print(string_01[-3])  # l
+# print(string_01[4])   # o
+# print(string_01[len(string_01) - 1])  # o
+# print(string_01[-1])  # o
+# print(string_01[-5])
+
+# string_01 = "Hello"
+# # new_string = string_01[0:4]
+# # print(new_string[0:4])
+# #
+# print(string_01[4:0:-1])
+# # string[<start>:<end>:<step>]
+# print(string_01[1:])
+# print(string_01[:2])
+# print(string_01[::2])  # H l o
+# print("678"[::-1])
+# print(input("num:")[::-1])
+# num_string = "567"
+
+
