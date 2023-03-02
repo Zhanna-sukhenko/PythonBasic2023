6345e636bdfbd0fd81225a460c6270d628f12eb9
zhanna <sukhenkozhanna@gmail.com>
2023-03-02 20:39:40 +0200

    invert dict append update

--- Lesson6_13_invert_dict.py
+++ Lesson6_13_invert_dict.py
@@ -1,20 +1,33 @@
+# my_dict = {
+#     "apple": ['malum', 'popum', 'popula'],
+#     "fruit": ['baca', 'bacca', 'popum'],
+#     "punishment": ['malum', 'multa']
+# }
+# swap_dict = {}
+# for k, v in my_dict.items():
+#     for v1 in v:
+#         if v1 in my_dict:
+#             print(swap_dict[v1].append([k]))
+#         else:
+#             swap_dict[v1] = [k]
+#
+# print("swap_dict", swap_dict)
+
+
 my_dict = {
-    "apple": ['malum', 'popum', 'popula'],
+    "apple": ['malum', 'pomum', 'popula'],
     "fruit": ['baca', 'bacca', 'popum'],
     "punishment": ['malum', 'multa']
 }
 swap_dict = {}
-for k, v in defaultdict.items():
-    for v1 in v:
-        if v1 in my_dict:
-            print(swap_dict[v1].append([k]))
+for k, v in my_dict.items():
+    for elem in v:
+        if elem in my_dict:
+            swap_dict[elem].append([k])
         else:
-            swap_dict[v1] = [k]
-
+            swap_dict[elem] = [k]
+swap_dict.update({"malum": ['apple', 'punishment']})
 print("swap_dict", swap_dict)
-
-
-
 
 # my_dict = {
 #     'apple': 'popum',
