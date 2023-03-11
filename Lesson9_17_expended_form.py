26d7b016a746348a77d113f128ddf73aa5456bef
zhanna <sukhenkozhanna@gmail.com>
2023-03-11 20:26:35 +0200

    def expanded_form() enumerate(str(num)[]) '+'.join(result[])

--- Lesson9_17_expended_form.py
+++ Lesson9_17_expended_form.py
@@ -1,12 +1,30 @@
-# try:
-#     num = int(input("input number bigger than 0: "))
-# except ValueError:
-#     print("incorrect number")
+# Напишить функцію, яка буде переводити число у розгорнутий вигляд
+# Вам буде надано число, і вам потрібно буде повернути його як рядок у розгорнутій формі.
+# Наприклад:
+# expanded_form(12) # повинно повернути '10 + 2'
+# expanded_form(42) # повинно повернути '40 + 2'
+# expanded_form(70304) # повинно повернути '70000 + 300 + 4'
+# Уважно: всі числа які будуть передаватись повинні бути більше 0, інакше треба показати помилку.
 
-def number_collaps(num):
-    if i in range(0, -1),
-        print(i*100, )
-        print(number_collaps())
-    except ValueError:
-        print("incorrect number")
+num = int(input("Enter a num more than 10: "))
+def expanded_form(num):
+    result = []
+
+    for index, digit in enumerate(str(num)[::-1]):
+        if int(digit) != 0:
+            result.append(digit + ('0' * index))
+        elif num == 0:
+            raise Exception("number is not > 0")
+    return ' + '.join(result[::-1])
+
+print(expanded_form(num))
+
+
+
+# def number_collaps(num):
+#     if i in range(0, -1),
+#         print(i*100, )
+#         print(number_collaps())
+#     except ValueError:
+#         print("incorrect number")
 
